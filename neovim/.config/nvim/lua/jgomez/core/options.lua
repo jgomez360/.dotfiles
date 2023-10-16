1fd899f9e9e33444a0ec26f1dccb4513c5f18367
jgomez360 <jag427@gmail.com>
2023-10-16 13:25:15 -0700

    move in insert mode

--- neovim/.config/nvim/lua/jgomez/core/options.lua
+++ neovim/.config/nvim/lua/jgomez/core/options.lua
@@ -8,6 +8,11 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- move around in insert mode
+keymap.set("i", "<C-j>", "<C-o>gj", { desc = "Move down in insert mode" })
+keymap.set("i", "<C-k>", "<C-o>gk", { desc = "Move up in insert mode" })
+keymap.set("i", "<C-h>", "<Left>", { desc = "Move left in insert mode" })
+keymap.set("i", "<C-l>", "<Right>", { desc = "Move right in insert mode" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -20,20 +25,20 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- navigate buffers
-keymap.set("n", "<TAB>", ":bnext<CR>", { desc = "Go to next buffer" }) --  go to next buffer
-keymap.set("n", "<S-TAB>", ":bprevious<CR>", { desc = "Go to previous buffer" }) --  go to previous buffer
+keymap.set("n", "<TAB>", ":bnext<CR>", { desc = "Go to next buffer" })              --  go to next buffer
+keymap.set("n", "<S-TAB>", ":bprevious<CR>", { desc = "Go to previous buffer" })    --  go to previous buffer
 keymap.set("n", "<leader>x", ":Bdelete<CR>", { desc = "Close the current buffer" }) --  close current buffer
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 keymap.set("n", "n", "nzzzv", { desc = "Keep search items in middle of screen" })
