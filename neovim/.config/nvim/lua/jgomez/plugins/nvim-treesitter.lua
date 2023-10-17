d92922e2cad52d782cbcba31fb164340d2cf16d1
jgomez360 <jag427@gmail.com>
2023-10-17 13:12:48 -0700

    disable ts yaml highlighting

--- neovim/.config/nvim/lua/jgomez/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/jgomez/plugins/nvim-treesitter.lua
@@ -14,6 +14,7 @@ return {
             treesitter.setup({ -- enable syntax highlighting
                 highlight = {
                     enable = true,
+                    disable = { "yaml" },
                 },
                 -- enable indentation
                 indent = { enable = true },
