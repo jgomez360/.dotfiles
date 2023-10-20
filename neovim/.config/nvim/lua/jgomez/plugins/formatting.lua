d40a9dc17cd4079a90046dae65575b84495acf47
jgomez360 <jag427@gmail.com>
2023-10-19 17:07:58 -0700

    setup prettierd_solitidy

--- neovim/.config/nvim/lua/jgomez/plugins/formatting.lua
+++ neovim/.config/nvim/lua/jgomez/plugins/formatting.lua
@@ -4,18 +4,42 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
+    local util = require("conform.util")
 
     conform.setup({
       formatters = {
-        prettierd = {
-          command = "prettierd --plugin=prettier-plugin-solidity",
+        prettierd_solidity = {
+          meta = {
+            url = "https://github.com/prettier-solidity/prettier-plugin-solidity",
+            description = "prettier plugin for automatically formatting solidity code",
+          },
+          command = util.from_node_modules("prettierd --plugin=prettier-plugin-solidity"),
+          args = { "$FILENAME" },
+          range_args = function(ctx)
+            local start_offset, end_offset = util.get_offsets_from_range(ctx.buf, ctx.range)
+            return { "$FILENAME", "--range-start=" .. start_offset, "--range-end=" .. end_offset }
+          end,
+          cwd = util.root_file({
+            -- https://prettier.io/docs/en/configuration.html
+            ".prettierrc",
+            ".prettierrc.json",
+            ".prettierrc.yml",
+            ".prettierrc.yaml",
+            ".prettierrc.json5",
+            ".prettierrc.js",
+            ".prettierrc.cjs",
+            ".prettierrc.toml",
+            "prettier.config.js",
+            "prettier.config.cjs",
+            "package.json",
+          }),
         },
       },
       formatters_by_ft = {
         beancount = { "bean_format" },
         json = { "prettierd" },
         yaml = { "prettierd" },
-        solidity = { "prettierd" },
+        solidity = { "prettierd_solidity" },
         toml = { "taplo" },
         markdown = { "prettierd" },
         rust = { "rustfmt" },
