c9fab22c37ad51fb960fd0283f5c6d19435ded19
jgomez360 <jag427@gmail.com>
2024-03-10 12:53:48 -0700

    update telescope config

--- neovim/.config/nvim/lua/jgomez/plugins/telescope.lua
+++ neovim/.config/nvim/lua/jgomez/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     "nvim-telescope/telescope-fzy-native.nvim",
     "nvim-tree/nvim-web-devicons",
     "ThePrimeagen/git-worktree.nvim",
+    "davvid/telescope-git-grep.nvim",
   },
   config = function()
     local git_worktree = require("git-worktree")
@@ -21,7 +22,7 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
           },
         },
         vimgrep_arguments = {
@@ -44,6 +45,7 @@ return {
     })
 
     telescope.load_extension("fzy_native")
+    telescope.load_extension("git_grep")
     telescope.load_extension("git_worktree")
 
     -- set keymaps
@@ -52,7 +54,7 @@ return {
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope git_grep live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set(
       "n",
