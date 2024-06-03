f92533753f2e32809ff90f5907dd3e74cb61749d
jgomez360 <jag427@gmail.com>
2024-06-03 09:34:29 -0700

    remove defaults added to nvim 0.10

--- neovim/.config/nvim/lua/jgomez/core/options.lua
+++ neovim/.config/nvim/lua/jgomez/core/options.lua
@@ -44,9 +44,6 @@ opt.wildmode = "longest,list,full"
 
 -- appearance
 
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true -- emits true (24-bit) colours in the terminal
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
