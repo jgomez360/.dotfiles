3bc27f43fb073e9f079bf58f8e50fed2a26846b4
jgomez360 <jag427@gmail.com>
2024-06-23 20:53:23 -0700

    add vim-huff

--- neovim/.config/nvim/lua/jgomez/plugins/init.lua
+++ neovim/.config/nvim/lua/jgomez/plugins/init.lua
@@ -6,4 +6,6 @@ return {
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 
   "sindrets/diffview.nvim", -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev
+
+  "wolflo/vim-huff", -- Syntax files for Huff. Doesn't do a ton, but enough to make Huff code readable
 }
